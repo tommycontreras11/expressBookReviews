4bc13400acf12c47ed8d147aa64fa69ed4e09d32
Tommy11 <tommycontreras34@gmail.com>
2024-08-01 12:10:01 -0400

    refactor middleware on index

--- final_project/index.js
+++ final_project/index.js
@@ -12,23 +12,23 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
-    if(req.session.authorization) {
-        const token = req.session.authorization['accessToken']
+    if(req?.session?.authorization?.accessToken) {
+        const token = req?.session?.authorization?.accessToken
 
         jwt.verify(token, 'access', (err, user) => {
-            if(!err) {
+            if(user) {
                 req.user = user
                 next()
+            }else {
+                return res.status(403).json({ message: "User not authenticated" })
             }
-
-            return res.status(403).json({ message: "User not authenticated" })
         })
     } else {
         return res.status(403).json({ message: "User not logged in" });
     }
 });
  
-const PORT =5001;
+const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
